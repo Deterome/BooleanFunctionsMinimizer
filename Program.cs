45f76469ecc8206e5ab7d4a56ba7a8c4e2f574eb
Deterome <rodion5rodion@gmail.com>
2024-03-23 22:56:40 +0300

    Added function that makes CNF

--- Program.cs
+++ Program.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections.Specialized;
 using System.Diagnostics.Tracing;
 using System.Dynamic;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using NewCollections.MultiValueDictionary;
 using NewValues.MarkableValue;
+using SymbolsConstants;
 
 namespace MLITA {
 
     class MLITA_LAB2 {
-        const char MERGED_BITS_SYMBOL = 'X';
-        const char BIT_ONE_SYMBOL = '1';
-        const char BIT_ZERO_SYMBOL = '0';
 
         static void Main(string[] args) {
             Console.WriteLine("Enter list of numbers of unit sets of a Boolean function:");
@@ -21,6 +20,66 @@ namespace MLITA {
             }
 
             FindMinDNF(MakeNumbersListFromString(numbersString));
+        }
+
+         static void FindMinDNF(List<int> decConstituentsOfOnes) {
+            var binConstituentsOfOnes = ConvertNumbersFromDecToBin(decConstituentsOfOnes, out _);
+            var primeImplicants = GetPrimeImplicants(binConstituentsOfOnes);
+            var implicantTable = MakeImplicantTable(primeImplicants, binConstituentsOfOnes);
+            Console.WriteLine();
+            DrawImplicantTable(implicantTable, true);
+            Console.WriteLine();
+            var CNF = MakeCNFFromImplicantTable(implicantTable);
+        }
+
+        static string MinimizeCNF(string CNF) {
+            string minCNF = string.Empty;
+            
+            return minCNF;
+        }
+
+        static string MakeCNFFromImplicantTable(Dictionary<string, Dictionary<string, bool>> implicantTable) {
+            var implicants = implicantTable.Keys.ToList();
+            var constituents = implicantTable[implicants[0]].Keys.ToList();
+            Dictionary<char, Dictionary<string, bool>> namedImplicantTable = new();
+
+            List<char> namedImplicantTableKeys = new();
+            char variableChar = 'a';
+            foreach (var implicant in implicants) {
+                namedImplicantTable.Add(variableChar, new());
+                namedImplicantTableKeys.Add(variableChar);
+                foreach (var constituent in constituents) {
+                    namedImplicantTable[variableChar].Add(constituent, implicantTable[implicant][constituent]);
+                }
+                variableChar++;
+            }
+
+            string CNF = string.Empty;
+            foreach (var constituent in constituents) {
+                if (CNF != string.Empty) {
+                    CNF += MlitaSymbols.DISJUNCTION_SYMBOL;
+                }
+                List<char> operandsList = new();
+                foreach (var variable in namedImplicantTableKeys) {
+                    if (namedImplicantTable[variable][constituent]) {
+                        operandsList.Add(variable);
+                    }
+                }
+                if (operandsList.Count == 1) {
+                    CNF += operandsList[0];
+                } else if (operandsList.Count > 1) {
+                    CNF += MlitaSymbols.OPENING_PARENTHESIS_SYMBOL;
+                    for (int operandId = 0; operandId < operandsList.Count; operandId++)  {
+                        CNF += operandsList[operandId];
+                        if (operandId != operandsList.Count - 1) {
+                            CNF += MlitaSymbols.CONJUNCTION_SYMBOL;
+                        }
+                    }
+                    CNF += MlitaSymbols.CLOSING_PARENTHESIS_SYMBOL;
+                }
+            }
+
+            return CNF;
         }
 
         static List<int> MakeNumbersListFromString(string numbersString) {
@@ -40,7 +99,7 @@ namespace MLITA {
             var binaryNumbersList = decNumbersList.Select(decNum => {
                 var binaryNumStr = Convert.ToString(decNum, 2);
                 if (binaryNumStr.Length < numberOfBits) {
-                    binaryNumStr = new String(BIT_ZERO_SYMBOL, (int)numberOfBits-binaryNumStr.Length) + binaryNumStr;
+                    binaryNumStr = new String(MlitaSymbols.BIT_ZERO_SYMBOL, (int)numberOfBits-binaryNumStr.Length) + binaryNumStr;
                 } 
                 return binaryNumStr;
                 }).ToList();
@@ -49,76 +108,70 @@ namespace MLITA {
             return binaryNumbersList;
         }
 
-        static void MakeImplicantTable(List<string> primeImplicants, List<string> constituentsOfOne) {
-            List<List<bool>> tableMatrix = new();
+        static void DrawImplicantTable(Dictionary<string, Dictionary<string, bool>> implicantTable, bool namedImplicants = false) {
+            var primeImplicants = implicantTable.Keys.ToList();
+            var constituentsOfOne = implicantTable[primeImplicants[0]].Keys.ToList();
             int bitsPerNum = constituentsOfOne[0].Length;
-
-            for (int primeImpId = 0; primeImpId < primeImplicants.Count; primeImpId++) {
-                tableMatrix.Add(new List<bool>());
-                for (int constitOfOneId = 0; constitOfOneId < constituentsOfOne.Count; constitOfOneId++) {
-                    bool isConstituentCovered = true;
-                    for (int bitId = 0; bitId < bitsPerNum; bitId++) {
-                        if (primeImplicants[primeImpId][bitId] == MERGED_BITS_SYMBOL) {
-                            continue;
-                        } else if (primeImplicants[primeImpId][bitId] != constituentsOfOne[constitOfOneId][bitId]){
-                            isConstituentCovered = false;
-                        }
-                    }
-                    tableMatrix[primeImpId].Add(isConstituentCovered);
-                }
-            }
-
-
-            const char ROW_SYMBOL = '_';
-            const char COL_SYMBOL = '|';
-            const char COVERED_CONSTITUENT_SYMBOL = '+';
-            const char UNCOVERED_CONSTITUENT_SYMBOL = ROW_SYMBOL;
             int spaceArountCell = 1;
-            Console.WriteLine();
-            Console.Write(new string(ROW_SYMBOL, bitsPerNum + spaceArountCell*2) + COL_SYMBOL);
+            Console.Write(new string(MlitaSymbols.ROW_SYMBOL, bitsPerNum + spaceArountCell*2) + MlitaSymbols.COL_SYMBOL);
             foreach (var constituent in constituentsOfOne) {
-                Console.Write(new String(ROW_SYMBOL, spaceArountCell) + constituent + new String(ROW_SYMBOL, spaceArountCell) + COL_SYMBOL);
+                Console.Write(new String(MlitaSymbols.ROW_SYMBOL, spaceArountCell) + constituent + new String(MlitaSymbols.ROW_SYMBOL, spaceArountCell) + MlitaSymbols.COL_SYMBOL);
+            }
+            if (namedImplicants) {
+                Console.Write(new string(MlitaSymbols.ROW_SYMBOL, 1 + spaceArountCell*2) + MlitaSymbols.COL_SYMBOL);
             }
             Console.WriteLine();
             int row = 0, col = 0;
+            char currentImplicantName = 'a';
             foreach (var implicant in primeImplicants) {
-                Console.Write(new String(ROW_SYMBOL, spaceArountCell) + implicant + new String(ROW_SYMBOL, spaceArountCell) + COL_SYMBOL);
-                while (col < tableMatrix[row].Count) {
-                    Console.Write(new String(ROW_SYMBOL, spaceArountCell + (int)Math.Ceiling((Convert.ToDouble(bitsPerNum)-1)/2)));
-                    if (tableMatrix[row][col]) {
-                        Console.Write(COVERED_CONSTITUENT_SYMBOL);
+                Console.Write(new String(MlitaSymbols.ROW_SYMBOL, spaceArountCell) + implicant + new String(MlitaSymbols.ROW_SYMBOL, spaceArountCell) + MlitaSymbols.COL_SYMBOL);
+                foreach(var constituent in constituentsOfOne) {
+                    Console.Write(new String(MlitaSymbols.ROW_SYMBOL, spaceArountCell + (int)Math.Ceiling((Convert.ToDouble(bitsPerNum)-1)/2)));
+                    if (implicantTable[implicant][constituent]) {
+                        Console.Write(MlitaSymbols.COVERED_CONSTITUENT_SYMBOL);
                     } else {
-                        Console.Write(UNCOVERED_CONSTITUENT_SYMBOL);
+                        Console.Write(MlitaSymbols.UNCOVERED_CONSTITUENT_SYMBOL);
                     }
-                    Console.Write(new String(ROW_SYMBOL, spaceArountCell + (int)Math.Floor((Convert.ToDouble(bitsPerNum)-1)/2)) + COL_SYMBOL);
+                    Console.Write(new String(MlitaSymbols.ROW_SYMBOL, spaceArountCell + (int)Math.Floor((Convert.ToDouble(bitsPerNum)-1)/2)) + MlitaSymbols.COL_SYMBOL);
                     col++;
                 }
+                Console.Write(new string(MlitaSymbols.ROW_SYMBOL, spaceArountCell) + new string(currentImplicantName, 1) + new string(MlitaSymbols.ROW_SYMBOL, spaceArountCell) + MlitaSymbols.COL_SYMBOL);
                 Console.WriteLine();
                 col = 0;
                 row++;
+                currentImplicantName++;
             }
         }
 
-        static void FindMinDNF(List<int> decConstituentsOfOnes) {
-            var binConstituentsOfOnes = ConvertNumbersFromDecToBin(decConstituentsOfOnes, out _);
-            var PrimeImplicants = GetPrimeImplicants(binConstituentsOfOnes);
-            MakeImplicantTable(PrimeImplicants, binConstituentsOfOnes);
-            // 3) Производим попарное сравнение элементов соседних групп, при этом помечаем те 
-            //    бинарные числа, которые нашли свою пару
-            // 4) С помощью метода Петрика находим тупиковую ДНФ
+        static Dictionary<string, Dictionary<string, bool>> MakeImplicantTable(List<string> primeImplicants, List<string> constituentsOfOne) {
+            Dictionary<string, Dictionary<string, bool>> implicantTable = new();
+            var bitsPerNum = constituentsOfOne[0].Length;
+            foreach (var implicant in primeImplicants) {
+                implicantTable.Add(implicant, new Dictionary<string, bool>());
+                foreach (var constituent in constituentsOfOne) {
+                    bool isConstituentCovered = true;
+                    for (int bitId = 0; bitId < bitsPerNum; bitId++) {
+                        if (implicant[bitId] == MlitaSymbols.MERGED_BITS_SYMBOL) {
+                            continue;
+                        } else if (implicant[bitId] != constituent[bitId]){
+                            isConstituentCovered = false;
+                        }
+                    }
+                    implicantTable[implicant].Add(constituent, isConstituentCovered);
+                }
+            }
+            return implicantTable;
         }
 
         static List<string> GetPrimeImplicants(List<string> binConstituentsOfOnes) {
             List<string> primeImplicants = new();
             var bitsPerElement = binConstituentsOfOnes[0].Length;
-
-            // пахнет рекурсией...
             var mergeElements = binConstituentsOfOnes;
             do {
 
             MultiValueDictionary<uint, MarkableValue<string>> mergeElementsGroups = new();
             foreach(var mergeElement in mergeElements) {
-                var numberOfOnesInElement = CountSymbolsInString(mergeElement, BIT_ONE_SYMBOL);
+                var numberOfOnesInElement = CountSymbolsInString(mergeElement, MlitaSymbols.BIT_ONE_SYMBOL);
                 mergeElementsGroups.Add(numberOfOnesInElement, new MarkableValue<string>(mergeElement));
             }
             List<uint> mergeElementsGroupsKeys = mergeElementsGroups.Keys.ToList();
@@ -144,11 +197,11 @@ namespace MLITA {
                             string mergedElement = curGroupMergeEl; // инициализируем склеенный элемент одним из исходных элементов
                             for (int bitId = 0; bitId < bitsPerElement; bitId++) {
                                 if (curGroupMergeEl[bitId] != nextGroupMergeEl[bitId]) {
-                                    if (countOfDifferentBits > 1 || curGroupMergeEl[bitId] == MERGED_BITS_SYMBOL || nextGroupMergeEl[bitId] == MERGED_BITS_SYMBOL ) { // склеиваем только те элементы, которые отличаются на один бит и имеют "x"-ы в одинаковых позициях
+                                    if (countOfDifferentBits > 1 || curGroupMergeEl[bitId] == MlitaSymbols.MERGED_BITS_SYMBOL || nextGroupMergeEl[bitId] == MlitaSymbols.MERGED_BITS_SYMBOL ) { // склеиваем только те элементы, которые отличаются на один бит и имеют "x"-ы в одинаковых позициях
                                         isElementsMergeable = false;
                                         break;
                                     }
-                                    mergedElement = mergedElement.Remove(bitId, 1).Insert(bitId, new String(MERGED_BITS_SYMBOL, 1));
+                                    mergedElement = mergedElement.Remove(bitId, 1).Insert(bitId, new String(MlitaSymbols.MERGED_BITS_SYMBOL, 1));
                                     isElementsMergeable = true;
                                     countOfDifferentBits++;
                                 }
---
+++ SymbolsConstants.cs
@@ -0,0 +1,16 @@
+namespace SymbolsConstants {
+    struct MlitaSymbols {
+        public const char MERGED_BITS_SYMBOL = '-';
+        public const char BIT_ONE_SYMBOL = '1';
+        public const char BIT_ZERO_SYMBOL = '0';
+        public const char ROW_SYMBOL = '_';
+        public const char COL_SYMBOL = '|';
+        public const char COVERED_CONSTITUENT_SYMBOL = '+';
+        public const char UNCOVERED_CONSTITUENT_SYMBOL = ROW_SYMBOL;
+        public const char OPENING_PARENTHESIS_SYMBOL = '(';
+        public const char CLOSING_PARENTHESIS_SYMBOL = ')';
+        public const char DISJUNCTION_SYMBOL = '*';
+        public const char CONJUNCTION_SYMBOL = '+';
+    }
+        
+}
